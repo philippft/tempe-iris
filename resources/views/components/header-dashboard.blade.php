b6a2b6c42cf70010e6a30d3227d21512ecb5f4d4
aurell-ev <tinklerel@gmail.com>
2026-06-24 18:31:25 +0800

    perbaiki header dashboard

--- resources/views/components/header-dashboard.blade.php
+++ resources/views/components/header-dashboard.blade.php
@@ -20,18 +20,11 @@
     </div>
 
     <div class="bg-white rounded-2xl px-4 py-2 flex items-center gap-2 shadow-md max-w-fit">
-        <img
-            src="{{ auth()->user()->profile
-                ? asset('storage/' . auth()->user()->profile)
-                : asset('images/default-profile.png') }}"
-            alt="Foto Profil"
-            class="size-6 rounded-full outline-black object-cover"
-        >
         <div class="text-left">
-            <h3 class="text-judul text-base font-bold leading-tight">
+            <h3 class="text-judul text-lg font-bold leading-tight">
                 {{ auth()->user()->username }}
             </h3>
-            <p class="text-dark-grey text-xs uppercase tracking-wide">
+            <p class="text-dark-grey text-base uppercase tracking-wide">
                 {{ auth()->user()->prodi }}
             </p>
         </div>
