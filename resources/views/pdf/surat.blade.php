50a3b21f94a47e3ca1ba46e7f166b72d29371ef3
philippft <dzosfphilip@gmail.com>
2026-06-25 06:16:01 +0800

    feat: feat pdf file

---
+++ resources/views/pdf/surat.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Surat Permohonan Peminjaman Inventaris</title>
+    <style>
+        @page {
+            margin: 1.2in 1in 1in 1in;
+        }
+
+        body {
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            font-size: 14px;
+            line-height: 1.5;
+            color: #000;
+        }
+
+        .content {
+            width: 100%;
+        }
+
+        .info-table {
+            width: 100%;
+            margin-bottom: 25px;
+            border-collapse: collapse;
+        }
+
+        .info-table td {
+            padding: 2px 0;
+            vertical-align: top;
+        }
+
+        /* Isi Surat */
+        .isi-surat {
+            text-align: justify;
+            margin-bottom: 20px;
+        }
+
+        /* Agenda/Jadwal Kegiatan */
+        .jadwal-container {
+            margin-left: 20px;
+            margin-bottom: 20px;
+        }
+
+        .jadwal-title {
+            font-weight: bold;
+            margin-bottom: 2px;
+        }
+
+        .jadwal-detail {
+            margin-left: 15px;
+            margin-bottom: 10px;
+        }
+
+        .jadwal-detail table td {
+            padding: 2px 0;
+        }
+
+        /* Bagian Tanda Tangan */
+        .ttd-container {
+            width: 100%;
+            margin-top: 40px;
+            border-collapse: collapse;
+        }
+
+        .ttd-container td {
+            text-align: center;
+            vertical-align: top;
+            width: 50%;
+        }
+
+        .space-ttd {
+            height: 75px;
+        }
+
+        .stempel-img {
+            height: 85px;
+            margin-top: -10px;
+            margin-bottom: -15px;
+        }
+
+        .nama-terang {
+            font-weight: bold;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="content">
+        <table class="info-table">
+            <tr>
+                <td style="width: 12%;">Nomor</td>
+                <td style="width: 3%;">:</td>
+                <td style="width: 50%;">SURAT/2023/INV/042</td>
+                <td style="text-align: right; width: 35%;">14 Oktober 2023</td>
+            </tr>
+            <tr>
+                <td>Lampiran</td>
+                <td>:</td>
+                <td colspan="2">1 (Satu)</td>
+            </tr>
+            <tr>
+                <td>Hal</td>
+                <td>:</td>
+                <td colspan="2">Permohonan Peminjaman Inventaris</td>
+            </tr>
+        </table>
+
+        <div style="margin-bottom: 25px;">
+            Yth. <strong>Badan Eksekutif Mahasiswa Fakultas Matematika dan Ilmu Pengetahuan Alam<br>
+                Universitas Udayana</strong><br>
+            di Jimbaran
+        </div>
+
+        <div style="margin-bottom: 15px;">
+            <u>Dengan Hormat,</u>
+        </div>
+
+        <div class="isi-surat">
+            Dalam rangka melaksanakan kegiatan Workshop Sharing Insight, Tech, and Action Knowledge Session (Workshop
+            SINTAKS), yang diselenggarakan oleh SIC Program Studi Informatika Fakultas MIPA Universitas Udayana, maka
+            kami bermaksud untuk mengajukan Permohonan Peminjaman Inventaris (terlampir). Adapun kegiatan tersebut akan
+            diselenggarakan pada:
+        </div>
+
+        <div class="jadwal-container">
+            <div class="jadwal-title">1. Gladi H-1</div>
+            <div class="jadwal-detail">
+                <table cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td style="width: 100px;">Hari, tanggal</td>
+                        <td style="width: 15px;">:</td>
+                        <td>Kamis, 26 Maret 2026</td>
+                    </tr>
+                    <tr>
+                        <td>Waktu</td>
+                        <td>:</td>
+                        <td>13.00 – 19.00 WITA</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="jadwal-title">2. Pelaksanaan Kegiatan</div>
+            <div class="jadwal-detail">
+                <table cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td style="width: 100px;">Hari, tanggal</td>
+                        <td style="width: 15px;">:</td>
+                        <td>Jumat, 27 Maret 2026</td>
+                    </tr>
+                    <tr>
+                        <td>Waktu</td>
+                        <td>:</td>
+                        <td>08.00 – Selesai WITA</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="isi-surat">
+            Demikian surat peminjaman ini kami sampaikan. Atas kerja sama dan dukungan yang diberikan, kami ucapkan
+            terima kasih.
+        </div>
+
+        <table style="width: 100%; border-collapse: collapse; margin-top: 30px;">
+            <tr>
+                <td style="width: 55%;"></td>
+                <td style="width: 45%; text-align: center; vertical-align: top;">
+                    Panitia Pelaksana<br>
+                    Workshop SINTAKS 1<br>
+                    Universitas Udayana
+
+                    <div class="space-ttd"></div>
+
+                    <div>
+                        <span class="nama-terang">Nama Ketua Panitia</span><br>
+                        NIM. 2408561000
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+</body>
+
+</html>
