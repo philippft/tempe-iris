a8e587f4f2e94bd7a3202ae4ff1af69a95d42dfb
gungmasmay <you@example.com>
2026-06-25 16:18:08 +0800

    dashboard admin test

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,18 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Dashboard')
+@section('title', 'Dashboard Admin') 
 
 @section('content')
 
-<x-header-dashboard/>
+<x-header-dashboard />
 
-<div class="space-y-8">
-
-<form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit" class="text-red-500 hover:text-red-700 font-bold">
-        Logout
-    </button>
-</form>
+<section class="">
+    <h2 class="text-base font-bold mb-4">STATISTIK PEMINJAMAN</h2>
+</section>
 
 @endsection
