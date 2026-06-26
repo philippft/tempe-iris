4cb7a1a71fb35a0048ca2232c3e343070ef07e93
philippft <dzosfphilip@gmail.com>
2026-06-26 22:16:09 +0800

    fix: add peminjaman in petinggi controller

--- app/Http/Controllers/PetinggiDashboardController.php
+++ app/Http/Controllers/PetinggiDashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Surat;
 
 class PetinggiDashboardController extends Controller
 {
     public function petinggiDashboard () 
     {
+        $suratMasuk = Surat::whereHas('detailPeminjaman.inventaris', function ($q) {
+            $q->where('id_user', auth()->id());
+        })->get();
+        
+
         return view('petinggi.dashboard');
     }
 }
