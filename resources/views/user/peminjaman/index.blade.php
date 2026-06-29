e92130dafb0fcf9e8603ffb55ce4fc8ceddbcdd0
gungmasmay <gungmas6@gmail.com>
2026-06-29 15:23:37 +0800

    page dashboard peminjaman user fix empty table dan tombol tambah peminjaman

--- resources/views/user/peminjaman/index.blade.php
+++ resources/views/user/peminjaman/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="space-y-8 p-8">
     {{-- Heading --}}
-    <div class="flex justify-between items-start">
+    <div class="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <div>
             <h1 class="text-4xl font-extrabold text-judul">
                 Daftar Peminjaman Inventaris
@@ -15,9 +15,13 @@
                 Kelola dan pantau status peminjaman inventaris Anda.
             </p>
         </div>
-        <x-button href="{{ route('user.peminjaman.create') }}">
+        <a href="{{ route('user.peminjaman.create') }}"
+                    class="text-white bg-primary-hover px-4 py-2.5 text-base font-bold rounded-lg shadow-sm hover:bg-primary transition flex items-center gap-2">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 15H11V11H15V9H11V5H9V9H5V11H9V15ZM10 20C8.61667 20 7.31667 19.7375 6.1 19.2125C4.88333 18.6875 3.825 17.975 2.925 17.075C2.025 16.175 1.3125 15.1167 0.7875 13.9C0.2625 12.6833 0 11.3833 0 10C0 8.61667 0.2625 7.31667 0.7875 6.1C1.3125 4.88333 2.025 3.825 2.925 2.925C3.825 2.025 4.88333 1.3125 6.1 0.7875C7.31667 0.2625 8.61667 0 10 0C11.3833 0 12.6833 0.2625 13.9 0.7875C15.1167 1.3125 16.175 2.025 17.075 2.925C17.975 3.825 18.6875 4.88333 19.2125 6.1C19.7375 7.31667 20 8.61667 20 10C20 11.3833 19.7375 12.6833 19.2125 13.9C18.6875 15.1167 17.975 16.175 17.075 17.075C16.175 17.975 15.1167 18.6875 13.9 19.2125C12.6833 19.7375 11.3833 20 10 20ZM10 18C12.2333 18 14.125 17.225 15.675 15.675C17.225 14.125 18 12.2333 18 10C18 7.76667 17.225 5.875 15.675 4.325C14.125 2.775 12.2333 2 10 2C7.76667 2 5.875 2.775 4.325 4.325C2.775 5.875 2 7.76667 2 10C2 12.2333 2.775 14.125 4.325 15.675C5.875 17.225 7.76667 18 10 18Z" fill="white"/>
+                </svg>
             Tambah Peminjaman
-        </x-button>
+        </a>
     </div>
 
     {{-- Statistik --}}
@@ -104,7 +108,7 @@
             headerBg="bg-primary-hover"
             headerClass="text-bg-light font-bold text-base uppercase"
         >
-            @foreach($peminjaman as $surat)
+            @forelse($peminjaman as $surat)
                 <x-table-row>
                     <div class="justify-center text-dark-grey">
                         {{ $loop->iteration }}
@@ -150,7 +154,9 @@
                         />
                     </div>
                 </x-table-row>
-            @endforeach
+                @empty
+                <x-table-empty message="Saat ini tidak ada peminjaman yang sedang dilakukan atau tidak ada data yang cocok dengan pencarian Anda."/>
+            @endforelse
         </x-table>
     </x-container>
 </div>
